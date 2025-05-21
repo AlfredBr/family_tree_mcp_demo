d4b84a7d0e368a05657b2c5107738db8e0849f1a
Alfred Broderick <AlfredBr@outlook.com>
2025-05-20 18:34:07 -0700

    comments

--- mcp/FamilyTools.cs
+++ mcp/FamilyTools.cs
@@ -8,14 +8,14 @@ namespace FamilyTreeApp;
 [McpServerToolType]
 public static class FamilyTools
 {
-    [McpServerTool, Description("Get a list of people in a family.")]
+    [McpServerTool, Description("Get a list of all of the people in a family.")]
     public static async Task<string> GetFamily(FamilyService familyService)
     {
         var family = await familyService.GetFamily();
         return JsonSerializer.Serialize(family);
     }
 
-    [McpServerTool, Description("Get a member of the family by id.")]
+    [McpServerTool, Description("Get a particular member of the family by id.")]
     public static async Task<string?> GetPerson(
         FamilyService familyService,
         [Description("The id of the person in the family")] string id
