eb34c7b190569adb0436834166d099ae06ef04b6
Alfred Broderick <AlfredBr@outlook.com>
2025-06-11 23:18:03 -0400

    Add PlaceOfBirth property to Person class
    Updated the `Person` class in the `FamilyTreeApp` namespace to include a new required property, `PlaceOfBirth`, of type `string`. The JSON property name for this field is "placeOfBirth".

--- models/Person.cs
+++ models/Person.cs
@@ -10,6 +10,7 @@ public class Person
 	[JsonPropertyName("id")] public required string Id { get; set; }
 	[JsonPropertyName("name")] public required string Name { get; set; }
 	[JsonPropertyName("gender")] public required string Gender { get; set; }
+	[JsonPropertyName("placeOfBirth")] public required string PlaceOfBirth { get; set; }
 	[JsonPropertyName("yearOfBirth")] public required int YearOfBirth { get; set; }
 	[JsonPropertyName("parents")] public required List<string> Parents { get; set; }
 	[JsonPropertyName("spouses")] public required List<string> Spouses { get; set; }
